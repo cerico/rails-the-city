7984f311e33be99da8a128d5fb834f4ab0109b3f
cerico <garethrobertlee@gmail.com>
2020-10-26 20:07:30 +0000

    remove unneeded

--- app/javascript/packs/railway.js
+++ app/javascript/packs/railway.js
@@ -3,6 +3,5 @@ import 'regenerator-runtime/runtime'
 
 import WebpackerReact from 'webpacker-react'
 import Counter from 'components/Counter'
-import Railway from 'components/Railway'
 
-WebpackerReact.setup({ Counter, Railway })
+WebpackerReact.setup({ Counter })
